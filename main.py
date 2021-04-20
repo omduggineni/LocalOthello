9f7c6721323da0f74d7f6884bf2d9762c28c71d0
Om Duggineni <omduggineni@gmail.com>
2021-04-19 21:29:06 -0400

    updated cmdline arguments

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ cmd.add_argument("-t", "--time", "--time-limit", required=False, default=2, help
 cmd.add_argument("-d", "--edax-difficulty", required=False, default=100, help="AI difficulty (0-100, default=100, only applies if using \"edax\")", type=int)
 cmd.add_argument("-e", "--edax", "--edax-path", required=False, default=".", help="path to folder/directory containing edax binary (only necessary if NOT in current directory) - please name the binary \"edax\" with no filename extension and make sure data/eval.dat is in the same directory as the binary")
 cmd.add_argument("-m", "--num-games", "--multiple-games", required=False, default=None, type=int, help="enable multi-game mode (prints win/loss statistics after playing multiple games)")
-cmd.add_argument("--machine-readable", help="machine-readable output", action="store_true", default=False, required=False)
+cmd.add_argument("-r", "--machine-readable", help="machine-readable output", action="store_true", default=False, required=False)
 
 args = cmd.parse_args()
 
