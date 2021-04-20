c262942178ba670f3d203caf2be786a2de629650
Om Duggineni <omduggineni@gmail.com>
2021-04-20 10:00:34 -0400

    added othelloresource.py import functionality

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import run
 import argparse
+import importlib
 
 cmd = argparse.ArgumentParser()
+cmd.add_argument("-o", "--othello-resource", required=True, help="path to othelloresource.py (no .py extension!)")
 cmd.add_argument("-1", "--player-one", required=False, default="console", help="first player (filename or \"console\", \"edax\", default: \"console\")")
 cmd.add_argument("-2", "--player-two", required=False, default="console", help="second player (filename or \"console\", \"edax\", default: \"console\")")
 cmd.add_argument("-t", "--time", "--time-limit", required=False, default=2, help="time limit (seconds, default=2)", type=int)
@@ -11,5 +13,7 @@ cmd.add_argument("-m", "--num-games", "--multiple-games", required=False, defaul
 cmd.add_argument("-r", "--machine-readable", help="machine-readable output", action="store_true", default=False, required=False)
 
 args = cmd.parse_args()
+othello_resource = importlib.__import__(args.othello_resource)
+print(othello_resource.possible_moves("........"*3+"...xo..."+"...ox..."+"........"*3, "x"))
 
 print(args)
