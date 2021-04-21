92128310bfb0ebf03c66466146a8439930dd4823
Om Duggineni <omduggineni@gmail.com>
2021-04-21 13:56:47 -0400

    improved "random" strategy speed (by making it inline)

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import multiprocessing as mp
 import sys
 import time
+import random
 
 def load_program_from_file(filename):
     from importlib import machinery as importlib_machinery
@@ -10,12 +11,12 @@ def load_program_from_file(filename):
     loader.exec_module(module)
     return module
 
+# Returns a tuple. 
+# The first element contains the program's module, 
+# the second contains a boolean which specifies whether or not to also load othelloresource.py and send it to the program 
+# (necessary for any Othello programs bundled with this application)
 def load_program(name):
-    if name == "random":
-        from runners import random_runner
-        return random_runner, True
-    else:
-        return load_program_from_file(name), False
+    return load_program_from_file(name), False
 
 def default_possible_moves(board, token):
     print("error - you don't define a possible_moves method")
@@ -37,7 +38,7 @@ def load_othelloresource(path):
 
 def program_caller(player_name, board_state, player_xo, best_move, still_running, othello_resource_path, ready):
     player, provide_othello_resource = load_program(player_name)
-    strat = player.Strategy()
+    strat = player.Strategy() #your IDE may complain about this but it's fine
     if provide_othello_resource:
         othello_resource = load_othelloresource(othello_resource_path)
         ready.value = 1
@@ -63,6 +64,10 @@ def run_program(board_state, player_xo, player_name, time_limit, othello_resourc
                 print("ERROR: Not a valid move")
             except EOFError:
                 print("ERROR: Not a valid move")
+    elif player_name == "random":
+        othello_resource = load_othelloresource(othello_resource_path)
+        possible_moves = othello_resource.possible_moves(board_state, player_xo)
+        return random.sample(possible_moves, 1)[0]
 
     best_move = mp.Value("i")
     best_move.value = -1
--- runners/random_runner.py
+++
@@ -1,6 +0,0 @@
-import random
-
-class Strategy:
-    logging = True
-    def best_strategy(self, board, player, best_move, still_running, othello_resource=None):
-        best_move.value = random.choice(othello_resource.possible_moves(board, player))
