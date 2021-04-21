6f0798a5f0d601c3fbbe4554655f378b5715ee9f
Om Duggineni <omduggineni@gmail.com>
2021-04-21 10:43:06 -0400

    got random to work

--- main.py
+++ main.py
@@ -1,35 +1,59 @@
 import run
 import argparse
-import importlib.machinery
+from importlib import machinery as importlib_machinery
 import types
 import sys
+import random
 
-cmd = argparse.ArgumentParser()
-cmd.add_argument("-o", "--othello-resource", required=True, help="path to othelloresource.py (you don't have to name it that)")
-cmd.add_argument("-1", "--player-one", required=False, default="console", help="first player (filename or \"console\", \"edax\", default: \"console\")")
-cmd.add_argument("-2", "--player-two", required=False, default="console", help="second player (filename or \"console\", \"edax\", default: \"console\")")
-cmd.add_argument("-t", "--time", "--time-limit", required=False, default=2, help="time limit (seconds, default=2)", type=int)
-cmd.add_argument("-d", "--edax-difficulty", required=False, default=100, help="AI difficulty (0-100, default=100, only applies if using \"edax\")", type=int)
-cmd.add_argument("-e", "--edax", "--edax-path", required=False, default=".", help="path to folder/directory containing edax binary (only necessary if NOT in current directory) - please name the binary \"edax\" with no filename extension and make sure data/eval.dat is in the same directory as the binary")
-cmd.add_argument("-m", "--num-games", "--multiple-games", required=False, default=1, type=int, help="enable multi-game mode (prints win/loss statistics after playing multiple games)")
-cmd.add_argument("-r", "--machine-readable", help="machine-readable output", action="store_true", default=False, required=False)
-
-args = cmd.parse_args()
-
-loader = importlib.machinery.SourceFileLoader("othello_resource", args.othello_resource)
-othello_resource = types.ModuleType(loader.name)
 def default_possible_moves(board, token):
-    print("error - you don't define a possible_moves method")
-    sys.exit(1)
-othello_resource.possible_moves = default_possible_moves
+        print("error - you don't define a possible_moves method")
+        sys.exit(1)
+        return []
 def default_make_move(board, token, index):
-    print("error - you don't define a make_move method")
-    sys.exit(1)
-othello_resource.make_move = default_make_move
-loader.exec_module(othello_resource)
+        print("error - you don't define a make_move method")
+        sys.exit(1)
+        return ""
+def load_othelloresource(path):
+    loader = importlib_machinery.SourceFileLoader("othello_resource", path)
+    module = types.ModuleType(loader.name)
+    module.possible_moves = default_possible_moves
+    module.make_move = default_make_move
+    loader.exec_module(module)
+    return module
+if __name__ == "__main__":
+    cmd = argparse.ArgumentParser()
+    cmd.add_argument("-o", "--othello-resource", required=True, help="path to othelloresource.py (you don't have to name it that)")
+    cmd.add_argument("-1", "--player-one", required=False, default="console", help="first player (filename or \"console\", \"edax\", default: \"console\")")
+    cmd.add_argument("-2", "--player-two", required=False, default="console", help="second player (filename or \"console\", \"edax\", default: \"console\")")
+    cmd.add_argument("-t", "--time", "--time-limit", required=False, default=2, help="time limit (seconds, default=2)", type=int)
+    cmd.add_argument("-d", "--edax-difficulty", required=False, default=100, help="AI difficulty (0-100, default=100, only applies if using \"edax\")", type=int)
+    cmd.add_argument("-e", "--edax", "--edax-path", required=False, default=".", help="path to folder/directory containing edax binary (only necessary if NOT in current directory) - please name the binary \"edax\" with no filename extension and make sure data/eval.dat is in the same directory as the binary")
+    cmd.add_argument("-m", "--num-games", "--multiple-games", required=False, default=1, type=int, help="enable multi-game mode (prints win/loss statistics after playing multiple games)")
+    cmd.add_argument("-r", "--machine-readable", help="machine-readable output", action="store_true", default=False, required=False)
 
-print(othello_resource.possible_moves("........"*3+"...xo..."+"...ox..."+"........"*3, "x"))
+    args = cmd.parse_args()
 
-time_limit = args.time
+    othello_resource_path = args.othello_resource
+    othello_resource = load_othelloresource(othello_resource_path)
 
-print(args)
+    time_limit = int(args.time)
+
+    swap_xo = {"x": "o", "o": "x", ".": "."}
+    player_dict = {"x": args.player_one, "o": args.player_two}
+    current_board = "...........................xo......ox..........................."
+    current_player = "x"
+    print(current_board)
+    while True:
+        if len(othello_resource.possible_moves(current_board, current_player)) == 0:
+            if len(othello_resource.possible_moves(current_board, swap_xo[current_player])) > 0:
+                current_player = swap_xo[current_player]
+                continue
+            else:
+                break
+
+        next_move = run.run_program(current_board, current_player, player_dict[current_player], time_limit, othello_resource_path)
+        print(current_player, next_move)
+        
+        current_board = othello_resource.make_move(current_board, current_player, next_move)
+        print(current_board)
+        current_player = swap_xo[current_player]
--- run.py
+++ run.py
@@ -1,16 +1,43 @@
 from types import SimpleNamespace
-import edax_runner
+from runners import console_runner, edax_runner, random_runner
+import importlib.machinery
+import types
+import multiprocessing as mp
 
 INVALID_FILENAME = -1
 
-program_cache = dict()
-program_cache["edax"] = edax_runner
+def load_program_from_file(filename):
+    loader = importlib.machinery.SourceFileLoader("othello_resource", filename)
+    module = types.ModuleType(loader.name)
+    loader.exec_module(module)
+    return module
 
-def run_program(name, board_state, player):
-    best_move = SimpleNamespace()
-    best_move.value = None
-    still_running = SimpleNamespace()
+def load_program(name):
+    if name == "edax":
+        return edax_runner, True
+    if name == "console":
+        return console_runner, True
+    if name == "random":
+        return random_runner, True
+    else:
+        return load_program_from_file(name), False
+
+def program_caller(player_name, board_state, player_xo, best_move, still_running, othello_resource_path):
+    player, provide_othello_resource = load_program(player_name)
+    strat = player.Strategy()
+    if provide_othello_resource:
+        strat.best_strategy(board_state, player_xo, best_move, still_running, othello_resource_path=othello_resource_path)
+    else:
+        strat.best_strategy(board_state, player_xo, best_move, still_running)
+
+def run_program(board_state, player_xo, player_name, time_limit, othello_resource_path):
+    best_move = mp.Value("i")
+    best_move.value = 0
+    still_running = mp.Value("i")
     still_running.value = 1
 
-    if player in program_cache:
-        ...
+    process = mp.Process(target=program_caller, args=(player_name, board_state, player_xo, best_move, still_running, othello_resource_path))
+    process.start()
+    process.join(2)
+    process.kill()
+    return best_move.value
