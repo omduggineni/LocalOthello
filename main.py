3c1a42594e0c7047e44aa8e8fdf022f43a6cd368
Om Duggineni <omduggineni@gmail.com>
2021-04-21 13:37:55 -0400

    updated cli interface

--- main.py
+++ main.py
@@ -37,7 +37,6 @@ if __name__ == "__main__":
     cmd.add_argument("-1", "--player-one", required=False, default="console", help="first player (filename or \"console\", \"random\", \"edax\", default: \"console\")")
     cmd.add_argument("-2", "--player-two", required=False, default="console", help="second player (filename or \"console\", \"random\", \"edax\", default: \"console\")")
     cmd.add_argument("-t", "--time", "--time-limit", required=False, default=2, help="time limit (seconds, default=2)", type=int)
-    cmd.add_argument("-e", "--edax", "--edax-path", required=False, default=".", help="path to folder/directory containing edax binary (only necessary if NOT in current directory) - please name the binary \"edax\" with no filename extension and make sure data/eval.dat is in the same directory as the binary")
     cmd.add_argument("-r", "--machine-readable", help="machine-readable output", action="store_true", default=False, required=False)
 
     args = cmd.parse_args()
